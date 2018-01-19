5dc5480f616016b7a6871c04b88f1e609f45297f
Your Name <you@example.com>
2018-01-19 15:05:33 +0100

    Added item templates

---
+++ ClientApp/components/item-templates.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+    <div class="row">
+        <h1 class="col-md-6">Przedmioty</h1>
+        <div class="col-md-3 col-center-items">
+            <input type="text" class="form-control mr-0" v-model="searchText">
+        </div>
+        <div class="col-md-3 col-center-items">
+            <button class="btn btn-primary btn-block add-item-btn" @click="goToAddItem">Dodaj Template</button>
+        </div>
+    </div>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th style="width:1px;">lp.</th>
+                <th style="width:1px;">Nazwa</th>
+                <th>Opis dodatkowy</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- <tr v-for="itemTemplate in filterItems(items)" v-bind:key="item.id">
+                <td>0</td>
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.description }}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm" @click="removeItem(item.id)">Usuń</button>
+                </td>
+            </tr> -->
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+import router from "../router.js";
+
+export default {
+    data() {
+        return {
+            itemTemplates: [ ],
+            searchText: "",
+        };
+    },
+
+    methods: {
+        filterItems(items) {
+            return items.filter((i) => this.searchText.length == 0 || (i.name + i.description + i.location).toLowerCase().includes(this.searchText.toLowerCase()));
+        },
+        goToAddItem() {
+            router.push("/add-item");
+        },
+        async removeItem(id) {
+            await this.$http.post('/api/ItemsTemplates/Remove', "id=" + id);
+            let response = await this.$http.get('/api/ItemsTemplates')
+            this.items = response.data;
+        }
+    },
+
+    computed: {
+        
+    },
+
+    async created() {
+        let response = await this.$http.get('/api/ItemsTemplates')
+        this.items = response.data;
+    }
+};
+</script>
+
+<style>
+
+</style>
--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -2,10 +2,12 @@ import HomePage from 'components/home-page'
 import Items from 'components/items'
 import AddItem from 'components/add-item'
 import Locations from 'components/locations'
+import ItemTemplates from 'components/item-templates'
 
 export const routes = [
     { path: '/', component: HomePage, display: 'Strona główna' },
     { path: '/items', component: Items, display: 'Wyposażenie' },
     { path: '/add-item', component: AddItem, display: 'Dodaj przedmiot' },
     { path: '/locations', component: Locations, display: 'Położenia' },
+    { path: '/item-templates', component: ItemTemplates, display: 'Typy Przedmiotow' }
 ]
